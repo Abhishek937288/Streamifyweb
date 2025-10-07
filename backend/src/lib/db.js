81ef873ca55dbb826066a54b233dd5bd8709742a
Abhishek <mankumbareabhi@gmail.com>
2025-10-07 21:05:51 +0530

    express setup

---
+++ backend/src/lib/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const url = process.env.MONGO_URI ;
+
+export const connectDb = async()=>{
+    try{
+        const conn = await mongoose.connect(url);
+        console.log(`MongoDB connected : ${conn.connection.host}`);
+    }catch(err){
+    console.log("errr while connectiong mongoDB",err);
+    process.exit(1); // failure one
+    }
+}
