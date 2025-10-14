2352c173fcd604fef0c75dc00f2ebfbe2300b82e
Abhishek <mankumbareabhi@gmail.com>
2025-10-14 21:10:28 +0530

    installed axios and tanstack

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,11 +5,20 @@ import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 
 const app = express();
 const port = process.env.PORT;
-app.use(cookieParser());
 
+const frontendUrl = process.env.FRONTEND_URL;
+
+app.use(cors({
+origin :frontendUrl,
+credentials: true
+}))
+
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
--- backend/src/utils/genSetToken.js
+++ backend/src/utils/genSetToken.js
@@ -6,7 +6,7 @@ const genAndSetToken = async (id, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true, // prevent XSS attacks,
-    sameSite: "strict", // prevent CSRF attacks
+    sameSite: "none", // prevent CSRF attacks
     secure: process.env.NODE_ENV === "production",
   });
 };
