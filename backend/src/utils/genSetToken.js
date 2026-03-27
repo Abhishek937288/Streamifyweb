d8763328361cc604c084e84813c506607a633359
Abhishek <mankumbareabhi@gmail.com>
2026-03-27 22:58:45 +0530

    fix cookie cross origin

--- backend/src/utils/genSetToken.js
+++ backend/src/utils/genSetToken.js
@@ -3,11 +3,12 @@ const secretKey = process.env.secretKey;
 
 const genAndSetToken = async (id, res) => {
   const token = jwt.sign({ id }, secretKey, { expiresIn: "15d" });
+
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true, // prevent XSS attacks,
-    sameSite: "strict", // prevent CSRF attacks
-    secure: process.env.NODE_ENVR === "production",
+    httpOnly: true,
+    sameSite: "none",   // ✅ allow cross-origin
+    secure: true,       // ✅ required for HTTPS (Vercel)
   });
 };
 
