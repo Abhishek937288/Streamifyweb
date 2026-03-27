736e01415f35ec43fe263ab476f8f7ded7819221
Abhishek <mankumbareabhi@gmail.com>
2026-03-27 21:31:47 +0530

    added changed required for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const backendUrl =
-  import.meta.env.MODE === "development"
-    ? import.meta.env.VITE_BACKEND_URL
-    : "/api";
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
 export const axiosInstance = axios.create({
   baseURL: backendUrl,
   withCredentials: true,
